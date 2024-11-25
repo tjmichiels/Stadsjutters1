87178a8289228666160aac44320e184852a35f2f
Jeroen <Jeroenpayens04@gmail.com>
2024-11-25 10:59:48 +0100

    Pagina's en foto's toegevoegd en nieuwe naam gegeven. menu items toegevoegd met naam en icoontjes.

--- Stadsjutters1/Instellingen.xaml.cs
+++ Stadsjutters1/Instellingen.xaml.cs
@@ -1,9 +1,28 @@
-namespace Stadsjutters1;
+using Microsoft.Maui.Controls;
+using System.Collections.Generic;
 
-public partial class Instellingen : ContentPage
+namespace Stadsjutters1
 {
-	public Instellingen()
-	{
-		InitializeComponent();
-	}
+    public partial class Instellingen : ContentPage
+    {
+        public Instellingen()
+        {
+            InitializeComponent();
+
+            var items = new List<Item>
+                {
+                    new() { Name = "Accountinstelling", Icon = "person_24dp_000000.svg" },
+                    new() { Name = "Notificatie-instellingen", Icon = "notifications_24dp_E8EAED" }
+                };
+
+            MyListView.ItemsSource = items;
+        }
+    }
+
+    public class Item
+    {
+        public string? Name { get; set; }
+        public string? Icon { get; set; } 
+    }
+
 }
